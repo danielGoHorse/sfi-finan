5e503f2c7da36e2bc4d6967f9ceaa1029bd431ad
Daniel Henrique Gotardo <daniel.gotardo@alliedbrasil.com.br>
2021-09-04 15:02:24 -0300

    detalhes-cliente

--- src/app/views/clientes/cliente-detalhes/cliente-detalhes.page.ts
+++ src/app/views/clientes/cliente-detalhes/cliente-detalhes.page.ts
@@ -1,8 +1,9 @@
 import { Subscription } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { Cliente } from 'src/app/interfaces/cliente';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-cliente-detalhes',
@@ -14,10 +15,13 @@ export class ClienteDetalhesPage implements OnInit {
   private clienteId: string = null;
   private clienteSubscription: Subscription;
   private cliente: Cliente = {};
+  private cordova: any;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private clienteService: ClienteService
+    private clienteService: ClienteService,
+    public router: Router,
+    public platform: Platform,
   ) {
     this.clienteId = this.activatedRoute.snapshot.params['id'];
     if (this.clienteId) this.loadCliente();
@@ -30,5 +34,21 @@ export class ClienteDetalhesPage implements OnInit {
 
   ngOnInit() {
   }
+
+  voltar() {
+    this.router.navigate(['/cliente-lista'])
+  }
+
+  chat() {
+
+    this.platform.ready().then(() => {
+
+      this.cordova.plugins.Whatsapp.send('+263783187321');
+
+    });
+
+  }
+
+
 
 }
